5dc487c90344530a650461e0569d243bb84e5930
Diana Vidal <dvidaal@gmail.com>
2026-03-14 17:40:18 +0100

    Added second 'rutinas' tab of the app

---
+++ app/routines.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Routines() {
+  return (
+    <>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>Mis Rutinas</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      ></View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    /*  flex: 1,
+      backgroundColor: "#f9fafb", */
+  },
+  header: {
+    backgroundColor: "#ffffff",
+    paddingTop: 60,
+    paddingBottom: 20,
+    paddingHorizontal: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#e5e7eb",
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "700",
+    color: "#111827",
+    marginTop: 8,
+  },
+});
