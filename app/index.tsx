3dc9cb42c85617445f8a741e4905a67c6cfc9811
Diana Vidal <dvidaal@gmail.com>
2026-03-14 17:39:50 +0100

    Added first 'entrenar' tab of the app

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,41 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>Selecciona una rutina</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      ></View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    /*  flex: 1,
+    backgroundColor: "#f9fafb", */
+  },
+  header: {
+    backgroundColor: "#ffffff",
+    paddingTop: 60,
+    paddingBottom: 20,
+    paddingHorizontal: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#e5e7eb",
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "700",
+    color: "#111827",
+    marginTop: 8,
+  },
+});
